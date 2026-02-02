899d5a5bf54b888eef7be953b5042be5b3165fac
iglbv <ilyagolubev2021@gmail.com>
2026-02-03 00:03:27 +0500

    add async redux

---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { IUser } from "../../models/IUser";
+import { fetchUsers } from "./ActionCreators";
+
+interface UserState {
+    users: IUser[]
+    isLoading: boolean;
+    error: string;
+}
+
+const initialState: UserState = {
+    users: [],
+    isLoading: false,
+    error: '',
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchUsers.fulfilled, (state, action: PayloadAction<IUser[]>) => {
+                state.isLoading = false;
+                state.error = '';
+                state.users = action.payload;
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error.message || 'Ошибка при загрузке пользователей'
+            });
+    }
+})
+
+export default userSlice.reducer;
